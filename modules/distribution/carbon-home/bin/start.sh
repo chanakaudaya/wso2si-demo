ce934a4dcefed90eaa9066cfa98eae40c4d6d803
niruhan <niruhanv@gmail.com>
2020-11-26 13:08:28 +0530

    Remove si files

--- modules/distribution/carbon-home/bin/start.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-nohup bash ./kafka/bin/zookeeper-server-start.sh ./kafka/config/zookeeper.properties >/dev/null 2>&1 &
-sleep 5
-nohup bash ./kafka/bin/kafka-server-start.sh ./kafka/config/server.properties >/dev/null 2>&1 &
-sh ./wso2si/bin/server.sh
